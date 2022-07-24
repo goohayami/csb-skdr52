f98856dac98cfd62098f905e1deaa924b6015ac1
Code Sandbox <hello@codesandbox.io>
2022-07-24 01:05:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c39d4c724fa3bc8652e38d63c8bcf064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ad4873ab5939a1e0aed9e4d016d3d2",
+    "url": "https://goohayami.github.io/csb-skdr52/index.html"
+  },
+  {
+    "revision": "ef30049be56d6bf25723",
+    "url": "https://goohayami.github.io/csb-skdr52/static/css/main.26495d14.chunk.css"
+  },
+  {
+    "revision": "eb0ff92cd0b0377e6d93",
+    "url": "https://goohayami.github.io/csb-skdr52/static/js/2.e97caeed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://goohayami.github.io/csb-skdr52/static/js/2.e97caeed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef30049be56d6bf25723",
+    "url": "https://goohayami.github.io/csb-skdr52/static/js/main.ec5aa6e6.chunk.js"
+  },
+  {
+    "revision": "c47e9929a4accf104f28",
+    "url": "https://goohayami.github.io/csb-skdr52/static/js/runtime-main.c02b70f4.js"
+  }
+]);
